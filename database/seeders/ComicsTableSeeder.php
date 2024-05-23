2827b33ffadbed830c8228836734257d7168ed9a
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-23 15:52:27 +0200

    feat: add seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,21 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //salvo i dati del file comics.php in una variabile
+        $comicArray = config('comics');
+        
+        //con ciclo foreach per ogni elemento dell'array creo una riga della tabella
+        foreach($comicArray as $singleComic){
+
+            $newComic = new Comic();
+            $newComic->title = $singleComic['title'];
+            $newComic->series = $singleComic['series'];
+            $newComic->description = $singleComic['description'];
+            $newComic->thumb = $singleComic['thumb'];
+            $newComic->type = $singleComic['type'];
+            $newComic->price = $singleComic['price'];
+            $newComic->sale_date = $singleComic['sale_date'];
+            $newComic->save();
+        }
     }
 }
