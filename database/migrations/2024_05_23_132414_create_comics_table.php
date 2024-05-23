5348f4121367887029e394bb294e9f7e6970e8c9
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-23 15:38:39 +0200

    feat: created migration

---
+++ database/migrations/2024_05_23_132414_create_comics_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('series', 100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('type', 100);
+            $table->double('price', 4,2);
+            $table->date('sale_date');
+            $table->timestamps();
+
+
+
+
+// 
+// 
+// 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
