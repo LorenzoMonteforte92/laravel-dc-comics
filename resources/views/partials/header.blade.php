0f544567eafa49b070332d574ef0077af7cb2def
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-23 16:28:08 +0200

    feat: add index route

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-12 text-center">
+                <h1>LaraComiX</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <ul class="d-flex justify-content-center gap-5 p-0">
+                    <li>
+                        <a href="{ route('home') }">Home</a>
+                    </li>
+                    <li>
+                        <a href="{ route('comics.index') }">Comic List</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
